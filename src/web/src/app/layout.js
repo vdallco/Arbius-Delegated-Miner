29a16902cb6b6f12c16efb847be3ed724d0d6387
Aniket Singh <thor98571@gmail.com>
2024-03-26 14:13:24 +0530

    fixed:renamed favicon.icon to icon.ico

--- src/web/src/app/layout.js
+++ src/web/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        </body>
     </html>
   );
 }
